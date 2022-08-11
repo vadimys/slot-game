45b0e784215678567b71904fa6d2620afb25ee0d
Vadym Zelinskyi <vadym.zelinskyi@rubyplay.com>
2022-08-11 16:48:47 +0200

    extends PIXI Application

--- src/Game.ts
+++ src/Game.ts
@@ -1,3 +1,23 @@
-export default class Game {
+import {Application} from 'pixi.js';
 
+export default class Game extends Application {
+    constructor() {
+        super();
+
+        this.init().then(() => this.ready());
+    }
+
+    public async init(): Promise<void> {
+        this.onResize()
+    }
+
+    private ready(): void {
+        document.body.appendChild(this.view)
+        window.addEventListener('resize', this.onResize.bind(this))
+    }
+
+    private onResize(): void {
+        this.renderer.resize(window.innerWidth + 2, window.innerHeight + 2)
+        this.stage.children.forEach((element: any) => element.resize(this.renderer.width, this.renderer.height))
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as PIXI from 'pixi.js';
 import Game from './Game';
 
+window.PIXI = PIXI;
 new Game();
